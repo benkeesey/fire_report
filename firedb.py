f03058ee393547328c2c5f56a465ad89ea4d6a9e
Ben Keesey <33181813+benkeesey@users.noreply.github.com>
2022-01-28 14:00:31 -0500

    update to db connection

--- firedb.py
+++ firedb.py
@@ -5,8 +5,8 @@ import mysql.connector
 def get_market_data():
 
     db_info = pd.read_pickle("user_info.pkl")
-    engine = sqlalchemy.create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}".format(host=db_info.loc['host'].value
-    , db='firedb', user=db_info.loc['user'].value, pw=db_info.loc['password'].value))
+    engine = sqlalchemy.create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}".format(host=db_info.loc['host']
+    , db='firedb', user=db_info.loc['user'], pw=db_info.loc['password']))
 
     yearly_market_df = pd.read_sql_table('yearly_market_df', engine) 
 
@@ -16,8 +16,8 @@ def get_market_data():
 
 def load_new_user_profile(user_var_series):
     db_info = pd.read_pickle("user_info.pkl")
-    mydb = mysql.connector.connect(host=db_info.loc['host'].value, database = 'firedb'
-    , user=db_info.loc['user'].value, passwd=db_info.loc['password'].value,use_pure=True)
+    mydb = mysql.connector.connect(host=db_info.loc['host'], database = 'firedb'
+    , user=db_info.loc['user'], passwd=db_info.loc['password'],use_pure=True)
 
     str_cols = str(tuple(user_var_series.index.values))
     str_cols = str_cols.replace("'","")
@@ -34,8 +34,8 @@ def load_new_user_profile(user_var_series):
 
 def return_user_profile(user_id):
     db_info = pd.read_pickle("user_info.pkl")
-    engine = sqlalchemy.create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}".format(host=db_info.loc['host'].value
-    , db='firedb', user=db_info.loc['user'].value, pw=db_info.loc['password'].value))
+    engine = sqlalchemy.create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}".format(host=db_info.loc['host']
+    , db='firedb', user=db_info.loc['user'], pw=db_info.loc['password']))
 
     user_id_df = pd.read_sql_query(f'SELECT * FROM user_fire_report_info WHERE user_id = "{user_id}"', engine)
     return user_id_df
